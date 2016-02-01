37eb39b7ad7cbfa1471b2075542bc4e3cc59ca04
jeffa <jeffa@unlocalhost.com>
2016-02-01 13:57:15 -0500

    refactored maps; added option for padding

--- lib/Encode/Wechsler.pm
+++ lib/Encode/Wechsler.pm
@@ -5,81 +5,13 @@ our $VERSION = '0.01';
 
 use Data::Dumper;
 
+my $i = 0;
 #{0, 1, 2, ..., 8, 9, a, b, ..., w} correspond to the bitstrings {'00000', '00001', '00010', ..., '11111'}.
-our %bits = (
-    0 => '00000',
-    1 => '00001',
-    2 => '00010',
-    3 => '00011',
-    4 => '00100',
-    5 => '00101',
-    6 => '00110',
-    7 => '00111',
-    8 => '01000',
-    9 => '01001',
-    a => '01010',
-    b => '01011',
-    c => '01100',
-    d => '01101',
-    e => '01110',
-    f => '01111',
-    g => '10000',
-    h => '10001',
-    i => '10010',
-    j => '10011',
-    k => '10100',
-    l => '10101',
-    m => '10110',
-    n => '10111',
-    o => '11000',
-    p => '11001',
-    q => '11010',
-    r => '11011',
-    s => '11100',
-    t => '11101',
-    u => '11110',
-    v => '11111',
-);
+our %bits = map { $_ => sprintf("%05d", unpack( 'B32', pack( 'N', $i++ ) ) ) } 0 .. 9, 'a' .. 'v';
 
-our %zero = (
-    'y0' => (0 x  4),
-    'y1' => (0 x  5),
-    'y2' => (0 x  6),
-    'y3' => (0 x  7),
-    'y4' => (0 x  8),
-    'y5' => (0 x  9),
-    'y6' => (0 x 10),
-    'y7' => (0 x 11),
-    'y8' => (0 x 12),
-    'y9' => (0 x 13),
-    'ya' => (0 x 14),
-    'yb' => (0 x 15),
-    'yc' => (0 x 16),
-    'yd' => (0 x 17),
-    'ye' => (0 x 18),
-    'yf' => (0 x 19),
-    'yg' => (0 x 20),
-    'yh' => (0 x 21),
-    'yi' => (0 x 22),
-    'yj' => (0 x 23),
-    'yk' => (0 x 24),
-    'yl' => (0 x 25),
-    'ym' => (0 x 26),
-    'yn' => (0 x 27),
-    'yo' => (0 x 28),
-    'yp' => (0 x 29),
-    'yq' => (0 x 30),
-    'yr' => (0 x 31),
-    'ys' => (0 x 32),
-    'yt' => (0 x 33),
-    'yu' => (0 x 34),
-    'yv' => (0 x 35),
-    'yw' => (0 x 36),
-    'yx' => (0 x 37),
-    'yy' => (0 x 38),
-    'yz' => (0 x 39),
-);
-
+$i = 4;
+# the symbols {'y0', 'y1', y2', ..., 'yx', 'yy', 'yz'} correspond to runs of between 4 and 39 consecutive '0's.
+our %zero = map { 'y' . $_ => (0 x  $i++ ) } 0 .. 9, 'a' .. 'z';
 
 sub new {
     my $self = shift;
@@ -99,13 +31,15 @@ sub decode {
     my @grid;
     $self->{_max_len} = 0;
     for my $part (split 'z', $format ) {
-        #$part = '00000' unless length( $part );
+
+        # pad left and right
+        $part = '0' . $part . '0' if $self->{pad};
 
         # We use the characters 'w' and 'x' to abbreviate '00' and '000', respectively.
         $part =~ s/w/00/g;
         $part =~ s/x/000/g;
 
-        # the symbols {'y0', 'y1', y2', ..., 'yx', 'yy', 'yz'} correspond to runs of between 4 and 39 consecutive '0's.
+        # replace consecutive zeros
         $part =~ s/(y.)/$zero{$1}/g;
 
         my $i = 0;
@@ -142,6 +76,8 @@ __END__
 
 Encode::Wechsler - Just another Wechsler encoder/decoder
 
+THIS MODULE IS AN ALPHA RELEASE!
+
 =head1 SYNOPSIS
 
 Object oriented interface:
@@ -155,6 +91,11 @@ Procedural interface:
   use Encode::Wechsler qw( wechsler_encode wechsler_decode );
 
 =head1 DESCRIPTION
+
+THIS MODULE IS AN ALPHA RELEASE!
+
+Wechsler encoding is used to describe game boards for Conway's
+Game of Life.
 
 =head1 METHODS
 
